44a0cc0510ae051579ef32593e9e6feb69502210
KarlStevenRequinto <lerdrobott@gmail.com>
2024-04-05 09:53:21 +0800

    minor fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ProjectCard = ({ index, name, description, tags, image, source_code_link,deploy_link }) => {
+const ProjectCard = ({ index, name, description, tags, image, source_code_link, deploy_link }) => {
     return (
         <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
             <Tilt
@@ -66,8 +66,8 @@ const Works = () => {
 
             <div className="w-full flex">
                 <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-                    Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described
-                    with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different
+                    Following projects showcase my skills and experience through real-world examples of my work. Each one is briefly described with
+                    links to code repositories and live demos that should reflect my ability to solve complex problems, work with different
                     technologies, and manage projects effectively.
                 </motion.p>
             </div>
